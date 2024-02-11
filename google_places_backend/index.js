bdd1ec7ddecea7aa489120d11864bbbb5d1601b7
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2024-02-11 16:55:01 +0530

    initial commit

---
+++ google_places_backend/index.js
@@ -0,0 +1,34 @@
+require ("dotenv").config()
+const express = require("express");
+const axios = require("axios");
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors());
+
+app.get('/all-places/:title/:loc', async (req, res) => {
+    const {title , loc }= req.params;
+    try {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${title}%20in%20${loc}&key=${process.env.API_KEY}`);
+      
+      if (!response.ok) {
+        throw new Error("Error generated");
+      }
+  
+      const data = await response.json();
+  
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET');
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  
+      res.json(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+app.listen(8000,() => {
+    console.log("Server is working at localhost:8000");
+})
